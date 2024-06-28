ea0a21b1d172cc92e939c4f824e5c43fb18712c6
Italo Silva <italo@maleldil.com>
2024-06-28 13:56:13 +0100

    metrics: Add instance-level metric calculation

--- src/whyqa/metrics.py
+++ src/whyqa/metrics.py
@@ -30,7 +30,7 @@ def _get_tokens(s: str) -> list[str]:
     return s.split()
 
 
-def calculate(instances: list[Instance]) -> Result:
+def calculate_dataset(instances: list[Instance]) -> Result:
     """Calculate token-level precision, recall and F1 scores, and Exact Match."""
     gold_len = 0
     pred_len = 0
@@ -57,5 +57,27 @@ def calculate(instances: list[Instance]) -> Result:
         else 0
     )
     em = equal_count / len(instances)
+
+    return Result(precision=precision, recall=recall, f1=f1, em=em)
+
+
+def calculate_sentence(gold: str, pred: str) -> Result:
+    gold_toks = _get_tokens(gold)
+    pred_toks = _get_tokens(pred)
+
+    pred_len = len(pred_toks)
+    gold_len = len(gold_toks)
+
+    common = Counter(gold_toks) & Counter(pred_toks)
+    common_tokens = sum(common.values())
+
+    precision = common_tokens / pred_len if pred_len != 0 else 0
+    recall = common_tokens / gold_len if gold_len != 0 else 0
+    f1 = (
+        (2 * precision * recall) / (precision + recall)
+        if precision + recall != 0
+        else 0
+    )
+    em = int(gold_toks == pred_toks)
 
     return Result(precision=precision, recall=recall, f1=f1, em=em)
